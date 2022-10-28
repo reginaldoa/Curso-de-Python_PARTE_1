e15a710dd9ecd5344f0172f4beff3a19d5bba4e5
Reginaldo Alves <reginaldoalvesti@outlook.com>
2022-10-27 23:59:32 -0300

    Desafios

---
+++ Desafios/Desafio_de_contadores.py
@@ -0,0 +1,22 @@
+#Desafio feito, antes de olhar a aula da resolução.
+'''
+ for / while
+  0 10
+  1  9
+  2  8
+  3  7
+  4  6
+  5  5
+  6  4
+  7  3
+  8  2
+'''
+
+
+contador = 0
+reduzindo = 10
+
+while reduzindo >= 2: #Dessa forma deu certo.
+    print(contador,reduzindo)
+    contador += 1
+    reduzindo -= 1
