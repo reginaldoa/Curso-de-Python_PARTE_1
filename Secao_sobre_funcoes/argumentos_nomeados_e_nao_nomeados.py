0abcb8ce6f933858f30d7f632cbfb94677656aa0
Reginaldo Alves <reginaldoalvesti@outlook.com>
2022-11-08 23:52:58 -0300

    Argumentos

---
+++ Secao_sobre_funcoes/argumentos_nomeados_e_nao_nomeados.py
@@ -0,0 +1,7 @@
+
+#Definição
+def soma (x , y , z): #parametros
+    print(f'{x=} y={y} || "x + y =', x + y)
+
+soma(2,2,2) #Caso não seja nomeado, deve seguir a ordem dos parametros
+soma(y= 10 , z= 0 , x =1 ) # Argumentos nomeados, dessa forma, posso colocar na ordem que eu quiser.
