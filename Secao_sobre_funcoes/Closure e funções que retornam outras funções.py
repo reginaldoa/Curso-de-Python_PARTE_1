97c1655d3646ef82ae711ce50e58e083aefa877b
Reginaldo Alves <reginaldoalvesti@outlook.com>
2022-11-17 23:58:19 -0300

    Closure

---
+++ Secao_sobre_funcoes/Closure e funções que retornam outras funções.py
@@ -0,0 +1,20 @@
+# Closure e funções que retornam outras funções
+
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}  {nome}'
+    return saudar
+
+    
+
+
+falar_bom_dia = criar_saudacao('bom dia')
+falar_boa_noite = criar_saudacao("Boa noite")
+print(falar_bom_dia('Reginaldo'))
+print(falar_boa_noite("Reginaldo"))
+
+
+for nome in ['Reginaldo', 'Renato', 'Neide']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
