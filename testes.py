3bfc6adee23c6126f6e3fcf12047efabd8d748f3
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-01-02 23:39:27 -0300

    filtro de dados

--- testes.py
+++
@@ -1,9 +0,0 @@
-#Exemplo de funções interligadas na outra.
-def nome(n):
-    def sobrenome(s):
-        return f'ola {n} {s}, tudo bem?'
-    return sobrenome
-
-
-saudacao = nome('Reginaldo')
-print(saudacao('Alves'))
