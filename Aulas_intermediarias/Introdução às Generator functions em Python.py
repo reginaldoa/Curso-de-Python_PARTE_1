3c6ff004d2d6a0eab9ee7e3faaf6b1236d6c95f2
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-01-11 23:18:10 -0300

    Introdução às Generator functions em Python

---
+++ Aulas_intermediarias/Introdução às Generator functions em Python.py
@@ -0,0 +1,20 @@
+#Introdução a generator functions em Python
+# Generator = (n for n in range(1000000)) 
+# O generator é uma função que dá pra pausar
+
+def generator( n= 0 , maximun = 10):
+   while True:
+        yield n # yeld Pausa a função
+        
+        n += 1
+
+
+        if n > maximun:
+            return 
+
+        
+
+gen = generator(maximun= 10000000)
+
+for n in gen:
+    print(n)
