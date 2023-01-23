b37925d179f03f3c8771d50a80ae7b45b50071ba
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-01-22 23:34:24 -0300

    packages

---
+++ aula99.py
@@ -0,0 +1,19 @@
+from sys import path
+
+#Importando de várias formas
+import aula99_package 
+from aula99_package.modulo import soma
+from aula99_package.modulo import soma
+from aula99_package import modulo
+from aula99_package.modulo import * # * significa ALL (TUDO)
+
+#print(path, sep = '\n')
+
+#print de acordo com as formas de importar
+print(aula99_package.modulo.soma(1,2))
+print(soma(1,2))
+print(modulo.soma(1,2))
+print(soma(1,2))
+print(variavel)
+print(teste)
+
---
+++ aula99_package/modulo.py
@@ -0,0 +1,13 @@
+__all__ = [ #O  que tiver fora do ALL, não será importado nesse caso.
+    'variavel',
+    'soma',
+    'teste'
+]
+
+variavel  = "Alguma coisa"
+
+def soma( x , y ):
+    return x + y
+
+teste = 'teste'
+
