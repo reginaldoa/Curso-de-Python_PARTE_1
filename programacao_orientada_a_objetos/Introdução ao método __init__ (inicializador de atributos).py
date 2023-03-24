b83e3b4b86b55e757aa638279bdd797d6db2a528
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-03-24 00:22:19 -0300

    nova aula

---
+++ programacao_orientada_a_objetos/Introdução ao método __init__ (inicializador de atributos).py
@@ -0,0 +1,13 @@
+class Pessoa():
+    def __init__(self, nome , sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+p1 = Pessoa('Reginaldo','Alves da Silva')
+print(p1.nome)
+print(p1.sobrenome)
+#p1.nome = "Reginaldo"
+#p1.sobrenome = "Alves da Silva"
+
+#print(p1.nome)
+#print(p1.sobrenome)
