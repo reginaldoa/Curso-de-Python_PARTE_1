337f07b801d3a9bfd570047fbb7b0b3e00288dff
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-08-18 00:20:34 -0300

    aulas sobre insert

--- Secao_sobre_base_de_dados/main.py
+++ Secao_sobre_base_de_dados/main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 ROOT_DIR =  Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE =  ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 
 connection =  sqlite3.connect(DB_FILE)
@@ -11,6 +12,29 @@ cursor = connection.cursor()
 
 
 #SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+
+connection.commit()
+
+#Registrar valores na coluna da tabela
+
+#CUIDADO AO FAZER ASSIM : SQL INJECTION
+cursor.execute(
+
+    f'INSERT INTO {TABLE_NAME} (id , name, weight) '
+    ' VALUES (NULL, "Reginaldo Alves", 96) , (NULL, "Regineide Alves", 70)'
+) 
+connection.commit()
+
+
+#cursor.executemany('')
 
 
 cursor.close()
